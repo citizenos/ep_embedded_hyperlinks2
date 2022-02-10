1fc251b303e3114dddd3bfdecf949061a01e883c
Mikk Andresen <mikk.andresen@gmail.com>
2022-02-10 14:39:33 +0200

    WIP: HTML Export

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
+const eejs = require('ep_etherpad-lite/node/eejs/');
 
 exports.eejsBlock_editbarMenuLeft = function(hook_name, args, cb) {
     args.content = args.content + eejs.require('ep_embedded_hyperlinks2/templates/editbarButtons.ejs');
     return cb();
-}
+};
 
 exports.eejsBlock_editorContainerBox = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_embedded_hyperlinks2/templates/popup.ejs", {}, module);
   return cb();
-}
+};
--- static/js/index.js
+++ static/js/index.js
@@ -1,56 +1,74 @@
-var $ = require('ep_etherpad-lite/static/js/rjquery').$;
+const $ = require('ep_etherpad-lite/static/js/rjquery').$;
 
-/* Bind the event handler to the toolbar buttons */
-exports.postAceInit = function(hook, context) {
+/**
+ * Bind the event handler to the toolbar buttons
+ *
+ * @see https://etherpad.org/doc/v1.8.16/#index_postaceinit
+ */
+exports.postAceInit = function (hook, context) {
     if (!$('#editorcontainerbox').hasClass('flex-layout')) {
         $.gritter.add({
-            title: "Error",
-            text: "Ep_embed_hyperlink2: Please upgrade to etherpad 1.9 for this plugin to work correctly",
+            title: 'Error',
+            text: 'Ep_embed_hyperlink2: Please upgrade to Etherpad 1.9 for this plugin to work correctly',
             sticky: true,
-            class_name: "error"
+            class_name: 'error'
         })
     }
+
     /* Event: User clicks editbar button */
-    $('.hyperlink-icon').on('click',function() {
+    $('.hyperlink-icon').on('click', function () {
         $('.hyperlink-dialog').toggleClass('popup-show');
         $('.hyperlink-dialog').css('left', $('.hyperlink-icon').offset().left - 12);
     });
+
     /* Event: User creates new hyperlink */
-    $('.hyperlink-save').on('click',function() {
-        var url = $('.hyperlink-url').val();
-        context.ace.callWithAce(function(ace) {
+    $('.hyperlink-save').on('click', function () {
+        const url = $('.hyperlink-url').val();
+
+        context.ace.callWithAce(function (ace) {
             ace.ace_doInsertLink(url);
         }, 'insertLink', true);
+
         $('.hyperlink-url').val('');
         $('.hyperlink-dialog').removeClass('popup-show');
     });
+
     /* User press Enter on url input */
-    $('.hyperlink-url').on("keyup", function(e)
-    {
-        if(e.keyCode == 13) // ENTER key
+    $('.hyperlink-url').on('keyup', function (e) {
+        if (e.code === 13 || e.keyCode === 13) // ENTER key
         {
-          $('.hyperlink-save').click();
+            $('.hyperlink-save').click();
         }
     });
-}
+};
 
-exports.aceAttribsToClasses = function(hook, context) {
-    if(context.key == 'url'){
-        var url = context.value;
-        return ['url-' + url ];
+/**
+ * @see https://etherpad.org/doc/v1.8.16/#index_aceattribstoclasses
+ */
+exports.aceAttribsToClasses = function (hook, context) {
+    if (context.key === 'url') {
+        const url = context.value;
+        return ['url-' + url];
     }
-}
+};
 
-/* Convert the classes into a tag */
-exports.aceCreateDomLine = function(name, context) {
-    var cls = context.cls;
-    var domline = context.domline;
-    var url = /(?:^| )url-(\S*)/.exec(cls);
-    var modifier = {};
-    if(url != null) {
+/**
+ * Convert the classes into a tag in the editor UI
+ *
+ * The element in the DOM will have class "url-${link}" and this will generate HTML <a tag woith href=${url}.
+ *
+ * https://etherpad.org/doc/v1.8.16/#index_acecreatedomline
+ */
+exports.aceCreateDomLine = function (name, context) {
+    console.log('aceCreateDomLine', name, context);
+
+    const cls = context.cls;
+    let url = /(?:^| )url-(\S*)/.exec(cls);
+    let modifier = {};
+    if (url != null) {
         url = url[1];
 
-        if(!(/^http:\/\//.test(url)) && !(/^https:\/\//.test(url))) {
+        if (!(/^http:\/\//.test(url)) && !(/^https:\/\//.test(url))) {
             url = "http://" + url;
         }
 
@@ -58,31 +76,37 @@ exports.aceCreateDomLine = function(name, context) {
             extraOpenTags: '<a href="' + url + '">',
             extraCloseTags: '</a>',
             cls: cls
-        }
+        };
+
         return modifier;
     }
     return [];
-}
+};
 
-/* I don't know what this does */
-exports.aceInitialized = function(hook, context) {
-    var editorInfo = context.editorInfo;
-    editorInfo.ace_doInsertLink = doInsertLink.bind(context);
-}
-
-function doInsertLink(url) {
-    var rep = this.rep,
-        documentAttributeManager = this.documentAttributeManager;
-    if(!(rep.selStart && rep.selEnd)) {
+function doInsertLink (url) {
+    const rep = this.rep;
+    const documentAttributeManager = this.documentAttributeManager;
+    if (!(rep.selStart && rep.selEnd)) {
         return;
     }
-    var url = ["url",url];
-    documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [url]);
+    const urlToInsert = ['url', url];
+    documentAttributeManager.setAttributesOnRange(rep.selStart, rep.selEnd, [urlToInsert]);
 }
 
-exports.collectContentPre = function(hook,context) {
-    var url = /(?:^| )url-(\S*)/.exec(context.cls);
-    if(url) {
-        context.cc.doAttrib(context.state,"url::" + url);
+/**
+ * @https://etherpad.org/doc/v1.8.16/#index_aceinitialized
+ */
+exports.aceInitialized = function (hook, context) {
+    const editorInfo = context.editorInfo;
+    editorInfo.ace_doInsertLink = doInsertLink.bind(context);
+};
+
+/**
+ * @see https://etherpad.org/doc/v1.8.16/#index_collectcontentpre
+ */
+exports.collectContentPre = function (hook, context) {
+    const url = /(?:^| )url-(\S*)/.exec(context.cls);
+    if (url) {
+        context.cc.doAttrib(context.state, 'url::' + url);
     }
-}
+};
